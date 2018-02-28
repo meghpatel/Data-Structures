1d329212248010477563671965c23f7ff7847402
meghpatel <patel.megh09@gmail.com>
2018-02-28 21:43:48 +0530

    Worked on primary operations on LL

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdio.h>
+#include<stdlib.h>
 
 //creating a structure for LinkedList
 
@@ -11,5 +12,15 @@ struct node
 
 int main()
 {
-	
+	struct node *newnode,*head;
+	newnode = (struct node*)malloc(sizeof(node)); 
+	head = newnode;
+	newnode->val = 5;
+	printf("%d",head->val);
+	struct node *newnode1;
+	newnode1 = (struct node*)malloc(sizeof(node));
+	newnode1->val = 4;
+	head->next = newnode1;
+	printf("%d",head->next->val);
+	printf("%d",newnode1->val);
 }
